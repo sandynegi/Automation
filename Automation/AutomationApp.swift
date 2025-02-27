3827d0655f6c393d715c2726678cf7d676fc9ec4
pawan <info@mobirizer.com>
2025-02-27 22:25:05 +0530

    Task | Initial Setup for Automation

---
+++ Automation/AutomationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomationApp.swift
+//  Automation
+//
+//  Created by pawan singh on 27/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AutomationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
